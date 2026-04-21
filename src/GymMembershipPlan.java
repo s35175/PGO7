4fc85bc02e08ba6a0c1bc679093b33ef68e30c8a
s35175 <s35175@pjwstk.edu.pl>
2026-04-21 11:42:27 +0200

    Update GymMembershipPlan.java

--- src/GymMembershipPlan.java
+++ src/GymMembershipPlan.java
@@ -25,4 +25,19 @@
     public boolean canFreeze() {
         return true;
     }
+}public class GymMembershipPlan extends MembershipPlan {
+
+    public GymMembershipPlan(String planCode, String clientName, int months, double baseMonthlyFee, boolean autoRenew) {
+        super(planCode, clientName, months, baseMonthlyFee, autoRenew);
+    }
+
+    @Override
+    public String getPlanType() {
+        return "Gym Membership";
+    }
+
+    @Override
+    public double calculateMonthlyNetPrice() {
+        return this.baseMonthlyFee; 
+    }
 }
